eee9257096a3258ed86732a5d3ea99e565ab69e1
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-11-14 20:56:32 -0300

    Move HOME bind for possible /home/../etc symlinks

--- src/scripts/_boot.sh
+++ src/scripts/_boot.sh
@@ -483,6 +483,22 @@ function _exec()
   _cmd_proot+=("-b /tmp")
   _cmd_proot+=("-b /sys")
 
+  # Set home binding
+  if [ "$HOME" != "/home/$(whoami)" ] && [ "$(id -u)" != "0" ]; then
+    _msg "HOME_BIND: Nested"
+    # Host home
+    _cmd_bwrap+=("--ro-bind \"/home/$(whoami)\" \"/home/$(whoami)\"")
+    _cmd_proot+=("-b \"/home/$(whoami)\"")
+    # User home
+    _cmd_bwrap+=("--bind \"$HOME\" \"$HOME\"")
+    _cmd_proot+=("-b \"$HOME\"")
+  else
+    _msg "HOME_BIND: Host"
+    # User home
+    _cmd_bwrap+=("--bind \"$HOME\" \"$HOME\"")
+    _cmd_proot+=("-b \"$HOME\"")
+  fi
+
   # Pulseaudio
   if [[ "$FIM_PERM_PULSEAUDIO" -eq 1 ]] &&
      [[ -n "$XDG_RUNTIME_DIR" ]]; then
@@ -638,22 +654,6 @@ function _exec()
   [ ! -f "/etc/group"         ] || _cmd_proot+=('-b "/etc/group"')
   [ ! -f "/etc/nsswitch.conf" ] || _cmd_proot+=('-b "/etc/nsswitch.conf"')
   [ ! -f "/etc/resolv.conf"   ] || _cmd_proot+=('-b "/etc/resolv.conf"')
-
-  # Set home binding
-  if [ "$HOME" != "/home/$(whoami)" ] && [ "$(id -u)" != "0" ]; then
-    _msg "HOME_BIND: Nested"
-    # Host home
-    _cmd_bwrap+=("--ro-bind \"/home/$(whoami)\" \"/home/$(whoami)\"")
-    _cmd_proot+=("-b \"/home/$(whoami)\"")
-    # User home
-    _cmd_bwrap+=("--bind \"$HOME\" \"$HOME\"")
-    _cmd_proot+=("-b \"$HOME\"")
-  else
-    _msg "HOME_BIND: Host"
-    # User home
-    _cmd_bwrap+=("--bind \"$HOME\" \"$HOME\"")
-    _cmd_proot+=("-b \"$HOME\"")
-  fi
 
   # Shell
   _cmd_bwrap+=("$FIM_FILE_BASH -c '${cmd[*]}'")

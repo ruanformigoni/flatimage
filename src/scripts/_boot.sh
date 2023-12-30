0e7fbfdf65e1264d3b31af887c5c3611335c33c2
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-12-30 15:55:38 -0300

    Fix missing magick

--- src/scripts/_boot.sh
+++ src/scripts/_boot.sh
@@ -1157,12 +1157,13 @@ function _main()
     "[" "b2sum" "base32" "base64" "basename" "cat" "chcon" "chgrp" "chmod" "chown" "chroot" "cksum"
     "comm" "cp" "csplit" "cut" "date" "dcgen" "dd" "df" "dir" "dircolors" "dirname" "du" "dwarfs"
     "echo" "env" "expand" "expr" "factor" "false" "fmt" "fold" "groups" "head" "hostid" "id" "join"
-    "kill" "link" "ln" "logname" "ls" "md5sum" "mkdir" "mkdwarfs" "mkfifo" "mknod" "mktemp" "mv"
-    "nice" "nl" "nohup" "nproc" "numfmt" "od" "overlayfs" "paste" "pathchk" "pr" "printenv" "printf"
-    "ptx" "pwd" "readlink" "realpath" "rm" "rmdir" "runcon" "seq" "sha1sum" "sha224sum" "sha256sum"
-    "sha384sum" "sha512sum" "shred" "shuf" "sleep" "sort" "split" "stat" "stty" "sum" "sync" "tac"
-    "tail" "tee" "test" "timeout" "touch" "tr" "true" "truncate" "tsort" "uname" "unexpand" "uniq"
-    "unlink" "uptime" "users" "vdir" "wc" "who" "whoami" "yes" "resize2fs" "mke2fs" "lsof"
+    "kill" "link" "ln" "logname" "ls" "magick" "md5sum" "mkdir" "mkdwarfs" "mkfifo" "mknod" "mktemp"
+    "mv" "nice" "nl" "nohup" "nproc" "numfmt" "od" "overlayfs" "paste" "pathchk" "pr" "printenv"
+    "printf" "ptx" "pwd" "readlink" "realpath" "rm" "rmdir" "runcon" "seq" "sha1sum" "sha224sum"
+    "sha256sum" "sha384sum" "sha512sum" "shred" "shuf" "sleep" "sort" "split" "stat" "stty" "sum"
+    "sync" "tac" "tail" "tee" "test" "timeout" "touch" "tr" "true" "truncate" "tsort" "uname"
+    "unexpand" "uniq" "unlink" "uptime" "users" "vdir" "wc" "who" "whoami" "yes" "resize2fs"
+    "mke2fs" "lsof"
   )
 
   # Setup daemon to unmount filesystems on exit

ab7b0bc7889c675685da69b8750b141454cbc998
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-05-15 11:50:08 -0300

    Include env and filesystem libraries

---
+++ src/cpp/std/env.hpp
@@ -0,0 +1,134 @@
+///
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : env
+///
+
+#pragma once
+
+#include <filesystem>
+#include <cstdlib>
+
+#include "../common.hpp"
+#include "../lib/log.hpp"
+
+// Environment variable handling {{{
+namespace ns_env
+{
+
+namespace fs = std::filesystem;
+
+// enum class Replace {{{
+enum class Replace
+{
+  Y,
+  N,
+}; // enum class Replace }}}
+
+// dir() {{{
+// Fetches a directory path from an environment variable
+// Tries to create if not exists
+inline fs::path dir(const char* name)
+{
+  // Get environment variable
+  const char * value = std::getenv(name) ;
+
+  // Check if variable exists
+  throw_if_not(value, "Variable '{}' not set"_fmt(name));
+
+  // Create if not exists
+  throw_if_not(fs::create_directory(value), "Could not create directory {}"_fmt(value));
+
+  return fs::canonical(value);
+} // dir() }}}
+
+// file() {{{
+// Fetches a file path from an environment variable
+// Checks if file exists
+inline fs::path file(const char* name)
+{
+  // Get environment variable
+  const char * value = std::getenv(name) ;
+
+  // Check if variable exists
+  throw_if_not(value, "Variable '{}' not set"_fmt(name));
+
+  // Create if not exists
+  throw_if_not(fs::exists(value), "File '{}' does not exist"_fmt(value));
+
+  return fs::canonical(value);
+} // file() }}}
+
+// set() {{{
+// Sets an environment variable
+template<ns_concept::AsString T, ns_concept::AsString U>
+void set(T&& name, U&& value, Replace replace)
+{
+  ns_log::debug("ENV: {} -> {}", name , value);
+  setenv(ns_string::to_string(name).c_str(), ns_string::to_string(value).c_str(), (replace == Replace::Y));
+} // set() }}}
+
+// prepend() {{{
+// Prepends 'extra' to an environment variable 'name'
+inline void prepend(const char* name, std::string const& extra)
+{
+  // Append to var
+  if ( const char* var_curr = std::getenv(name); var_curr )
+  {
+    ns_log::debug("ENV: {} -> {}", name, extra + var_curr);
+    setenv(name, std::string{extra + var_curr}.c_str(), 1);
+  } // if
+} // prepend() }}}
+
+// concat() {{{
+// Appends 'extra' to an environment variable 'name'
+inline void concat(const char* name, std::string const& extra)
+{
+  // Append to var
+  if ( const char* var_curr = std::getenv(name); var_curr )
+  {
+    setenv(name, std::string{var_curr + extra}.c_str(), 1);
+  } // if
+} // concat() }}}
+
+// set_mutual_exclusion() {{{
+// If first exists, sets first to value and unsets second
+// Else sets second to value
+inline void set_mutual_exclusion(const char* name1, const char* name2, const char* value)
+{
+  if ( std::getenv(name1) != nullptr )
+  {
+    setenv(name1, value, true);
+    unsetenv(name2);
+    return;
+  } // if
+
+  setenv(name2, value, true);
+} // set_mutual_exclusion() }}}
+
+// print() {{{
+// print an env variable
+inline void print(const char* name, std::ostream& os = std::cout)
+{
+  if ( const char* var = std::getenv(name); var )
+  {
+    os << var;
+  } // if
+} // print() }}}
+
+// get() {{{
+// Get an env variable
+inline const char* get(const char* name)
+{
+  return std::getenv(name);
+} // get() }}}
+
+// check() {{{
+// Checks if variable exists, else throws
+inline void check(const char* var)
+{
+  throw_if_not(get(var), "'{}' is unset"_fmt(var));
+} // check() }}}
+
+} // namespace ns_env }}}
+
+/* vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :*/
---
+++ src/cpp/std/filesystem.hpp
@@ -0,0 +1,175 @@
+///
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : path
+///
+
+#pragma once
+
+#include <filesystem>
+#include <numeric>
+#include <expected>
+
+#include "../common.hpp"
+
+namespace ns_fs
+{
+
+namespace fs = std::filesystem;
+
+// namespace ns_path {{{
+namespace ns_path
+{
+
+// struct Ret {{{
+template<typename T>
+struct Ret
+{
+  T _ret;
+  bool _bool;
+  std::string _msg;
+
+  Ret(T _ret, bool _bool, std::string _msg)
+    : _ret(_ret)
+    , _bool(_bool)
+    , _msg(_msg)
+  {}
+}; // struct: Ret 
+// }}}
+
+// should_throw() {{{
+template<bool _throw = true, typename U, std::convertible_to<std::string> T>
+Ret<U> should_throw(T&& t)
+{
+  if constexpr ( _throw )
+  {
+    "{}"_throw(t);
+  } // if
+
+  return Ret(U{}, false, t);
+} // should_throw() }}}
+
+// canonical() {{{
+// Try to make path canonical
+template<bool _throw = true> 
+Ret<fs::path> canonical(fs::path const& path)
+{
+  fs::path ret{path};
+
+  try
+  {
+    // Adjust for relative path
+    if (not ret.string().starts_with("/"))
+    {
+      ret = fs::path(fs::path{"./"} /= ret);
+    } // if
+    ret = fs::canonical(ret);
+  }
+  catch(std::exception const& e)
+  {
+    return should_throw<_throw, fs::path>("Could not make cannonical path for parent of '{}'"_fmt(path));
+  }
+
+  return Ret(ret, true, "");
+} // function: canonical }}}
+
+// file_self() {{{
+template<bool _throw = true> 
+Ret<fs::path> file_self()
+{
+  std::error_code ec;
+
+  auto path_file_self = fs::read_symlink("/proc/self/exe", ec);
+
+  if ( ec.value() != 0 )
+  {
+    return should_throw<_throw, fs::path>("Failed to fetch location of self");
+  } // if
+
+  return Ret(canonical<_throw>(path_file_self)._ret, true, "");
+} // file_self() }}}
+
+// dir_self() {{{
+template<bool _throw = true> 
+Ret<fs::path> dir_self()
+{
+  return Ret(file_self<_throw>()._ret.parent_path(), true, "");
+} // dir_self() }}}
+
+// file_exists() {{{
+// File exists and is a regular file
+template<bool _throw = true> 
+Ret<fs::path> file_exists(fs::path path_file)
+{
+  // Does not exist
+  if ( ! fs::exists(path_file) )
+  {
+    return should_throw<_throw, fs::path>("File '{}' does not exist"_fmt(path_file));
+  } // if
+
+  // Exists, but is not dir
+  if ( ! fs::is_regular_file(path_file) )
+  {
+    return should_throw<_throw, fs::path>("Path '{}' exists but is not a regular file"_fmt(path_file));
+  } // if
+
+  return Ret(canonical<_throw>(path_file)._ret, true, "");
+} // file_exists() }}}
+
+// file_name() {{{
+// Check if last component is a file name
+template<bool _throw> 
+Ret<std::string> file_name(fs::path const& path)
+{
+  std::string str_name = path.filename().string();
+
+  if ( str_name.empty() or str_name == "." or str_name == ".." )
+  {
+    return should_throw<_throw, std::string>("Empty file name for path {}"_fmt(path));
+  }
+
+  return Ret(str_name, true, "");
+} // function: file_name }}}
+
+// dir_create() {{{
+// Creates directories in 'path' if not exists
+template<bool _throw = true> 
+Ret<fs::path> dir_create(fs::path path_dir)
+{
+  if( fs::exists(path_dir) && ! fs::is_directory(path_dir) )
+  {
+    return should_throw<_throw, fs::path>("Path '{}' exists and is not a directory"_fmt(path_dir));
+  } // if
+
+  if ( ! fs::exists(path_dir) && ! fs::create_directories(path_dir) )
+  {
+    return should_throw<_throw, fs::path>("Failed to create directory '{}'"_fmt(path_dir));
+  } // if
+
+  return Ret(canonical<_throw>(path_dir)._ret, true, "");
+} // dir_create() }}}
+
+// dir_exists() {{{
+// File exists and is a directory
+template<bool _throw = true> 
+Ret<fs::path> dir_exists(fs::path path_dir)
+{
+  // Does not exist
+  if ( ! fs::exists(path_dir) )
+  {
+    return should_throw<_throw, fs::path>("Directory '{}' does not exist"_fmt(path_dir));
+  } // if
+
+  // Exists, but is not dir
+  if ( ! fs::is_directory(path_dir) )
+  {
+    return should_throw<_throw, fs::path>("Path '{}' exists but is not a directory"_fmt(path_dir));
+  } // if
+
+  return Ret(canonical<_throw>(path_dir)._ret, true, "");
+} // dir_exists() }}}
+
+} // namespace ns_path }}}
+
+} // namespace ns_fs
+
+/* vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :*/

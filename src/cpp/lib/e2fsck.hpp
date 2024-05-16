495b2b9c9ac4529941fb06c656e0c7cceacc1a1f
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-05-15 23:47:51 -0300

    Update

---
+++ src/cpp/lib/e2fsck.hpp
@@ -0,0 +1,47 @@
+///
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : e2fsck
+///
+
+#pragma once
+
+#include "subprocess.hpp"
+
+#include "../std/string.hpp"
+
+namespace ns_e2fsck
+{
+
+namespace
+{
+
+namespace fs = std::filesystem;
+
+}
+
+inline int check(fs::path const& path_file_image, uint64_t offset)
+{
+  // Check if image exists and is a regular file
+  ereturn_if(not fs::exists(path_file_image) or not fs::is_regular_file(path_file_image)
+    , "'{}' does not exist or is not a regular file"_fmt(path_file_image)
+    , -1
+  );
+
+  // Find command in PATH
+  auto opt_path_file_e2fsck = ns_subprocess::search_path("e2fsck");
+  ereturn_if(not opt_path_file_e2fsck.has_value(), "Could not find e2fsck", 1);
+
+  // Execute command
+  auto ret = ns_subprocess::Subprocess(*opt_path_file_e2fsck)
+    .with_args("-y")
+    .with_args(path_file_image.string() + "?offset=" + ns_string::to_string(offset))
+    .spawn(true);
+
+  if ( not ret.has_value() ) { ret = 1; }
+
+  return *ret;
+} // check
+
+} // namespace ns_e2fsck
+
+/* vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :*/

37e07d05511ede1ac74a9525299b0fd8ee2104df
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-10-14 02:56:23 -0300

    Bump flatimage version in examples

--- examples/steam.sh
+++ examples/steam.sh
@@ -29,7 +29,7 @@ function _main()
 
   # Download flatimage
   local image="./bin/arch.flatimage"
-  wget -q --show-progress --progress=dot:mega -O "$image" "https://github.com/ruanformigoni/flatimage/releases/download/v1.0.0/arch.flatimage"
+  wget -q --show-progress --progress=dot:mega -O "$image" "https://github.com/ruanformigoni/flatimage/releases/download/v1.0.1/arch.flatimage"
   chmod +x "$image"
 
   # Enable network
